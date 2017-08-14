d36e96ebce63c1dbc1f287cc9b3484bbb88ab58f
Mark Alston <malston@pivotal.io>
2017-08-14 15:30:18 -0600

    Add common.sh for reusuable functions

--- tasks/build-api-compatibility-check/task.sh
+++ tasks/build-api-compatibility-check/task.sh
@@ -14,8 +14,8 @@ echo "Version resource folder is [${VERSION_RESOURCE}]"
 cd ${ROOT_FOLDER}/${REPO_RESOURCE}
 
 # CURRENTLY WE ONLY SUPPORT JVM BASED PROJECTS OUT OF THE BOX
-[[ -f "${ROOT_FOLDER}/${TOOLS_RESOURCE}/tasks/build-and-upload/projectType/pipeline-jvm.sh" ]] && source "${ROOT_FOLDER}/${TOOLS_RESOURCE}/tasks/build-and-upload/projectType/pipeline-jvm.sh" || \
-    echo "No ${ROOT_FOLDER}/${TOOLS_RESOURCE}/tasks/build-and-upload/projectType/pipeline-jvm.sh found"
+[[ -f "${ROOT_FOLDER}/${TOOLS_RESOURCE}/tasks/build-api-compatibility-check/projectType/pipeline-jvm.sh" ]] && source "${ROOT_FOLDER}/${TOOLS_RESOURCE}/tasks/build-api-compatibility-check/projectType/pipeline-jvm.sh" || \
+    echo "No ${ROOT_FOLDER}/${TOOLS_RESOURCE}/tasks/build-api-compatibility-check/projectType/pipeline-jvm.sh found"
 #
 # export OUTPUT_FOLDER=$( outputFolder )
 # export TEST_REPORTS_FOLDER=$( testResultsAntPattern )
@@ -24,6 +24,8 @@ cd ${ROOT_FOLDER}/${REPO_RESOURCE}
 #
 # echo "Generating settings.xml / gradle properties for Maven in local m2"
 # source ${ROOT_FOLDER}/${TOOLS_RESOURCE}/tasks/generate-settings.sh
+
+source ${ROOT_FOLDER}/${TOOLS_RESOURCE}/tasks/common.sh
 
 export TERM=dumb
 
