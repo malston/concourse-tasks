2ca6f9063c97ff4df249616ada5036d45b949a76
Mark Alston <malston@pivotal.io>
2017-08-15 12:18:22 -0600

    Polish build-and-upload task

--- tasks/build-and-upload/task.sh
+++ tasks/build-and-upload/task.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+set -o errexit
+
+cwd="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 export ROOT_FOLDER=$( pwd )
 export REPO_RESOURCE=repo
 export TOOLS_RESOURCE=tools
@@ -10,18 +14,22 @@ echo "Root folder is [${ROOT_FOLDER}]"
 echo "Repo resource folder is [${REPO_RESOURCE}]"
 echo "Tools resource folder is [${TOOLS_RESOURCE}]"
 echo "Version resource folder is [${VERSION_RESOURCE}]"
+echo "Current working directory is [${cwd}]"
 
 echo "Retrieving version"
 export PIPELINE_VERSION=$( cat ${ROOT_FOLDER}/${VERSION_RESOURCE}/version )
 echo "Retrieved version is [${PIPELINE_VERSION}]"
 
 export CI="CONCOURSE"
+export ENVIRONMENT=BUILD
+
+source ${ROOT_FOLDER}/${TOOLS_RESOURCE}/tasks/common.sh
 
 cd ${ROOT_FOLDER}/${REPO_RESOURCE}
 
 # CURRENTLY WE ONLY SUPPORT JVM BASED PROJECTS OUT OF THE BOX
-[[ -f "${ROOT_FOLDER}/${TOOLS_RESOURCE}/tasks/build-and-upload/projectType/pipeline-jvm.sh" ]] && source "${ROOT_FOLDER}/${TOOLS_RESOURCE}/tasks/build-and-upload/projectType/pipeline-jvm.sh" || \
-    echo "No ${ROOT_FOLDER}/${TOOLS_RESOURCE}/tasks/build-and-upload/projectType/pipeline-jvm.sh found"
+[[ -f "${cwd}/projectType/pipeline-jvm.sh" ]] && source "${cwd}/projectType/pipeline-jvm.sh" || \
+    echo "No ${cwd}/projectType/pipeline-jvm.sh found"
 
 export OUTPUT_FOLDER=$( outputFolder )
 export TEST_REPORTS_FOLDER=$( testResultsAntPattern )
@@ -35,11 +43,6 @@ export TERM=dumb
 
 echo "Building and uploading the projects artifacts"
 cd ${ROOT_FOLDER}/${REPO_RESOURCE}
-
-export ENVIRONMENT=BUILD
-
-[[ -f "${__DIR}/pipeline.sh" ]] && source "${__DIR}/pipeline.sh" || \
-    echo "No pipeline.sh found"
 
 build
 
