56971b69915226ece2a9abb842f2686d6ca7e5ca
Mark Alston <malston@pivotal.io>
2017-08-15 10:45:55 -0600

    Add functions for deploying app

---
+++ tasks/test-deploy/projectType/pipeline-gradle.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+function retrieveGroupId() {
+    local result=$( ./gradlew groupId -q )
+    result=$( echo "${result}" | tail -1 )
+    echo "${result}"
+}
+
+function retrieveAppName() {
+    local result=$( ./gradlew artifactId -q )
+    result=$( echo "${result}" | tail -1 )
+    echo "${result}"
+}
--- tasks/test-deploy/task.sh
+++ tasks/test-deploy/task.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+set -o errexit
+
+__DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 export ROOT_FOLDER=$( pwd )
 export REPO_RESOURCE=repo
 export TOOLS_RESOURCE=tools
@@ -12,15 +16,18 @@ echo "Tools resource folder is [${TOOLS_RESOURCE}]"
 echo "Version resource folder is [${VERSION_RESOURCE}]"
 
 export CI="CONCOURSE"
+export ENVIRONMENT=TEST
 
 source ${ROOT_FOLDER}/${TOOLS_RESOURCE}/tasks/common.sh
+
+# CURRENTLY WE ONLY SUPPORT JVM BASED PROJECTS OUT OF THE BOX
+[[ -f "${__DIR}/projectType/pipeline-jvm.sh" ]] && source "${__DIR}/projectType/pipeline-jvm.sh" || \
+    echo "No ${__DIR}/projectType/pipeline-jvm.sh found"
 
 export TERM=dumb
 
 echo "Deploying the built application on test environment"
 cd ${ROOT_FOLDER}/${REPO_RESOURCE}
-
-export ENVIRONMENT=TEST
 
 [[ -f "${__DIR}/pipeline.sh" ]] && source "${__DIR}/pipeline.sh" || \
     echo "No pipeline.sh found"
